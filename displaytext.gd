15e38c4910935450498f6f01f776b340aa16596e
Aaron Klapatch <Aaron>
2023-11-29 20:25:53 -0500

    Fixed a bug or two with font resizing

--- displaytext.gd
+++ displaytext.gd
@@ -7,8 +7,17 @@ func get_font(font_name: String):
 	
 @onready var default_mod = self.self_modulate
 @onready var told_to_hide = false
+@onready var old_font_size = 0
+@onready var old_font = ""
 
 func _on_slides_display_text(words, font_size, font_align, font):
+	# Don't do anything if we're getting the same inputs are before
+	var new_bbcode = "[p align=" + font_align.to_lower() + "]" + words + "[/p]"
+	if new_bbcode == self.text and old_font_size == font_size and old_font == font:
+		return
+	old_font_size = font_size
+	old_font = font
+ 
 	var tween = self.create_tween()
 	if told_to_hide:
 		tween.tween_property(self, "self_modulate", default_mod, .5).set_trans(Tween.TRANS_LINEAR)
@@ -23,8 +32,8 @@ func _on_slides_display_text(words, font_size, font_align, font):
 		words = ""
 	if font_align == null:
 		font_align = "center"
-		
-	self.text = "[p align=" + font_align.to_lower() + "]" + words + "[/p]"
+	
+	self.text = new_bbcode
 	
 	if font != null:
 		var new_font = get_font("res://%s" % font)
@@ -45,5 +54,9 @@ func _on_slides_display_text(words, font_size, font_align, font):
 		# IMO, I won't do that for now.
 		var adjusted_font_size = get_parent().size.y * (float(font_size)/100.0)
 		self.add_theme_font_size_override("normal_font_size", int(adjusted_font_size))
-		
-	
+
+func _on_faderdisptexture_resized():
+	if old_font_size == null:
+		return
+	var adjusted_font_size = get_parent().size.y * (float(old_font_size)/100.0)
+	self.add_theme_font_size_override("normal_font_size", int(adjusted_font_size))
